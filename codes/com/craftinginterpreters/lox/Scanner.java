783c92e6c4614a7824651ea74631a2cd25b9d1dc
cihy23159 <cihy23159@g.nihon-u.ac.jp>
2025-12-02 10:31:16 +0900

    3

--- codes/com/craftinginterpreters/lox/Scanner.java
+++ codes/com/craftinginterpreters/lox/Scanner.java
@@ -36,45 +36,75 @@ class Scanner {
     }
 
     private void scanToken() {
-        char c = advance();
-        switch (c) {
-            case '(' -> addToken(LEFT_PAREN);
-            case ')' -> addToken(RIGHT_PAREN);
-            case '{' -> addToken(LEFT_BRACE);
-            case '}' -> addToken(RIGHT_BRACE);
-            case ',' -> addToken(COMMA);
-            case '.' -> addToken(DOT);
-            case '-' -> addToken(MINUS);
-            case '+' -> addToken(PLUS);
-            case ';' -> addToken(SEMICOLON);
-            case '*' -> addToken(STAR);
+      char c = advance();
+      switch (c) {
+        case '(': addToken(LEFT_PAREN); break;
+      case ')': addToken(RIGHT_PAREN); break;
+      case '{': addToken(LEFT_BRACE); break;
+      case '}': addToken(RIGHT_BRACE); break;
+      case ',': addToken(COMMA); break;
+      case '.': addToken(DOT); break;
+      case '-': addToken(MINUS); break;
+      case '+': addToken(PLUS); break;
+      case ';': addToken(SEMICOLON); break;
+      case '*': addToken(STAR); break; 
 
-            case '!' ->
-                addToken(match('=') ? BANG_EQUAL : BANG);
-            case '=' ->
-                addToken(match('=') ? EQUAL_EQUAL : EQUAL);
-            case '<' ->
-                addToken(match('=') ? LESS_EQUAL : LESS);
-            case '>' ->
-                addToken(match('=') ? GREATER_EQUAL : GREATER);
+        case '!':
+          addToken(match('=') ? BANG_EQUAL : BANG);
+          break;
+          case '=':
+          addToken(match('=') ? EQUAL_EQUAL : EQUAL);
+          break;
+          case '<':
+          addToken(match('=') ? LESS_EQUAL : LESS);
+          break;
+          case '>':
+          addToken(match('=') ? GREATER_EQUAL : GREATER);
+          break;
+        case '/': 
+          if (match('/')) {
+            while (peek() != '\n' && !isAtEnd()) advance();
+          } else {
+              addToken(SLASH);
+          }
+          break;
+        
 
-            case '/' -> {
-                if (match('/')) {
-                    while (peek() != '\n' && !isAtEnd()) advance();
-                } else {
-                    addToken(SLASH);
-                }
-            }
+        case ' ':
+        case '\r':
+        case '\t':
+          break;
+        case '\n':
+          line++;
+          break;
+        case '"':
+          string(); break;
 
-            // 空白
-            case ' ', '\r', '\t' -> {}
-            case '\n' -> line++;
+        default:
+          if (isDigit(c)) {
+            number();
+          } else {
+            Lox.error(line, "Unexpected character.");
+          }
+        break;
 
-            case '"' -> string();
-
-            default -> Lox.error(line, "Unexpected character.");
+            //default -> Lox.error(line, "Unexpected character.");
         }
     }
+      private void number() {
+    while (isDigit(peek())) advance();
+
+    // Look for a fractional part.
+    if (peek() == '.' && isDigit(peekNext())) {
+      // Consume the "."
+      advance();
+
+      while (isDigit(peek())) advance();
+    }
+
+    addToken(NUMBER,
+        Double.parseDouble(source.substring(start, current)));
+  }
 
     // ----- 文字列リテラル -----
 
@@ -106,9 +136,16 @@ class Scanner {
     }
 
     private char peek() {
-        if (isAtEnd()) return '\0';
-        return source.charAt(current);
+      if (isAtEnd()) return '\0';
+      return source.charAt(current);
     }
+    private char peekNext() {
+    if (current + 1 >= source.length()) return '\0';
+    return source.charAt(current + 1);
+  } 
+    private boolean isDigit(char c) {
+      return c >= '0' && c <= '9';
+    } 
 
     private char advance() {
         return source.charAt(current++);
